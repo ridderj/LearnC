2a8c00dd744f9d662d7b64748fe1190e4d8feb4f
ridderj <ridderj@oregonstate.edu>
2016-01-09 16:29:46 -0600

    Add IF

--- learnC.c
+++ learnC.c
@@ -5,8 +5,13 @@ int main()
    int decks;
    puts("Enter a number of decks");
    scanf("%i", &decks);
+   if (decks < 1) {
+      puts("Thats not valid");
+      return 1;
+   }
    
    
+   printf("There are %i cards\n", (decks * 52));
    puts("C Rocks!");
    return 0;
 }
